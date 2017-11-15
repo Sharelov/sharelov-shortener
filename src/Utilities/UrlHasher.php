05ee8a43f2268fad2b045e718fa787b5e5d86c28
Jorge Morales <jlm@effectix.net>
2017-11-15 12:39:07 -0500

    Made it more cryptographically secure
    It doesn't have to be super crypto secure, but since the hash is a unique key in the database, we want the randomness to be strong. There can be more improvements to this utility, but for now this is enough to satisfy our purposes.

--- src/Utilities/UrlHasher.php
+++ src/Utilities/UrlHasher.php
@@ -6,8 +6,22 @@ class UrlHasher
 {
     public function make($url, $length = 5)
     {
-        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-';
+        // define alphabet. separated in group types for clarity
+        $pool = '0123456789';
+        $pool .= 'abcdefghijklmnopqrstuvwxyz';
+        $pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $pool .= '_-~';
+        // initialize the hash var
+        $hash = "";
+        // count the length of the alphabet pool
+        $max = strlen($pool);
 
-        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+        // do the rounds selecting one character until we reach the specified length
+        for ($i = 0; $i < $length; ++$i) {
+            $hash .= $pool[random_int(0, $max - 1)];
+        }
+
+        // we are done, return the hash
+        return $hash;
     }
 }
