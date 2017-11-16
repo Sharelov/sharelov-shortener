1a913b4d714e46609f172ceab3494c04ee858e89
Jorge Morales <jlm@effectix.net>
2017-11-16 11:45:28 -0500

    PSR2 & PSR4 formatting

--- src/Facades/Shortener.php
+++ src/Facades/Shortener.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 namespace Sharelov\Shortener\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Shortener extends Facade {
-
-    protected static function getFacadeAccessor(){
+class Shortener extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
         return 'Shortener';
     }
-
 }
