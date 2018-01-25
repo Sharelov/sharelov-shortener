f782476194df60d30e40b1c32dece4accb16e989
Jorge Morales <jlm@effectix.net>
2018-01-25 09:24:05 -0400

    New methods and verification of whether or not softDeletes is enabled
    It will select the correct model based on whether or not that is set to true. 
    
    Added a method to deleteByHash that does what says in the label.
    
    Added a getter for the model. So you can grab the model after the softDeletes check.

--- src/Repositories/ShortLinkRepository.php
+++ src/Repositories/ShortLinkRepository.php
@@ -3,26 +3,53 @@
 namespace Sharelov\Shortener\Repositories;
 
 use Sharelov\Shortener\Models\ShortLink;
+use Sharelov\Shortener\Models\ShortLinkWithSoftDelete;
 
 class ShortLinkRepository
 {
+    /**
+     * Shortlinks model to be used in the repository
+     * @var null
+     */
+    protected $model = null;
+
+    public function __construct()
+    {
+        $this->model = new ShortLink;
+        if (config('enable_soft_deletes', false)) {
+            $this->model = new ShortLinkWithSoftDelete;
+        }
+        return $this;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+    
     public function create(array $data)
     {
-        return ShortLink::create($data);
+        return $this->model->create($data);
     }
 
     public function byUrl($url)
     {
-        return ShortLink::whereUrl($url)->first();
+        return $this->model->whereUrl($url)->first();
     }
 
     public function byHash($hash)
     {
-        return ShortLink::whereHash($hash)->first();
+        return $this->model->whereHash($hash)->first();
     }
 
     public function expired($link)
     {
         return $link && $link->expires && $link->expires_at->diffInDays(null, false) > 0;
     }
+
+    public function destroyByHash($hash)
+    {
+        return $this->byHash($hash)->destroy();
+    }
+
 }
